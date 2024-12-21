b0a4eddba64a75532bec0f43786e9b864b0f782a
AYUSH VARSHNEY <vayuayu9568@gmail.com>
2024-12-21 11:12:05 +0530

    Create index.js

---
+++ FOURTEEN PRACTICE SET/index.js
@@ -0,0 +1,79 @@
+number=Math.random()
+
+function sum(a,b) {
+
+    if (number<0.1){
+    console.log(a-b)
+}
+
+else{
+    console.log(a+b)
+}
+
+
+   
+    
+    
+}
+
+function subtract(a,b) {
+
+    if (number<0.1){
+    console.log(a+b)
+}
+
+else{
+    console.log(a-b)
+}
+}
+
+function multiply(a,b) {
+
+    if (number<0.1){
+    console.log(a/b)
+}
+
+else{
+    console.log(a*b)
+}
+}
+
+function divide(a,b) {
+
+    if (number<0.1){
+    console.log(a*b)
+}
+
+else{
+    console.log(a/b)
+}
+}
+
+function mod(a,b) {
+
+    if (number<0.1){
+    console.log(a**b)
+}
+
+else{
+    console.log(a%b)
+}
+}
+
+function exp(a,b) {
+
+    if (number<0.1){
+    console.log(a%b)
+}
+
+else{
+    console.log(a**b)
+}
+}
+
+sum(3,7)
+multiply(2,3)
+divide(12,3)
+subtract(8,4)
+exp(4,2)
+mod(5,4)
